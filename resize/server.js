0983cb35ae81a6576b313c2e440b045c0186b78f
Philip Scott <pscott@zeptohost.com>
2022-10-30 15:29:32 -0400

    Finish resize example

--- resize/server.js
+++ resize/server.js
@@ -14,13 +14,13 @@ app.get('/computer', async (req, res) => {
     res.send(computer);
     return;
   }
-  const resp = await axios.post('https://engine.hyperbeam.com/v0/vm', {}, {
+  const resp = await axios.post('https://enginetest.hyperbeam.com/v0/vm', {region: "EU", timeout: {offline: 10}}, {
     headers: { 'Authorization': `Bearer ${process.env.HB_API_KEY}` }
   });
   computer = resp.data;
   res.send(computer);
 });
 
-app.listen(8080, () => {
-  console.log('Server start at http://localhost:8080');
+app.listen(8081, () => {
+  console.log('Server start at http://localhost:8081');
 });
