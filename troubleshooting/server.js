a9fb465a1eee0d9c075780645c8853f14dec492a
Philip Scott <pscott@zeptohost.com>
2022-12-01 16:28:01 -0500

    add troubleshooting example

---
+++ troubleshooting/server.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+// Get a cloud computer object. If no object exists, create it.
+let computer
+app.get('/computer', async (req, res) => {
+  if (computer) {
+    res.send(computer)
+    return
+  }
+  const resp = await axios.post('https://engine.hyperbeam.com/v0/vm', {}, {
+    headers: { 'Authorization': `Bearer ${process.env.HB_API_KEY}` }
+  })
+  computer = resp.data
+  res.send(computer)
+})
+
+app.listen(8080, () => {
+  console.log('Server start at http://localhost:8080')
+})
